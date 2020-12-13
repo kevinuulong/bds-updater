5160bb4b3a72799903805253965204d5e52e8095
Kevin Long <kevin.uu.long@gmail.com>
2020-12-12 22:36:40 -0500

    v1.0.0

---
+++ index.js
@@ -0,0 +1,59 @@
+const got = require('got');
+const cheerio = require('cheerio');
+const path = require('path');
+const extract = require('extract-zip');
+const fs = require('fs-extra');
+const https = require('https');
+
+const config = require('./config.json');
+
+const pageUrl = "https://www.minecraft.net/en-us/download/server/bedrock";
+
+var zip;
+var output;
+
+got(pageUrl).then(res => {
+    console.log('🔗 Extracting latest download link')
+    const $ = cheerio.load(res.body);
+    const downloadUrl = $('.downloadlink')[0].attribs.href;
+    zip = `versions/${path.posix.basename(downloadUrl)}`;
+    var file = fs.WriteStream(zip);
+    output = path.resolve('versions/output/');
+
+    console.log('🔽 Downloading the latest version of BDS', zip);
+
+    https.get(downloadUrl, res => {
+        res.pipe(file);
+    })
+    file.on('finish', () => {
+        console.log('📦 Backing up');
+        config.doNotOverwrite.forEach(file => {
+            fs.move(path.resolve(config.serverPath, file), path.resolve('backup/', file), err => {
+                if (err) return console.error(err);
+                // console.log('Backed up:', file);
+            })
+        })
+        console.log('👽 Extracting');
+        async function extraction() {
+            try {
+                await extract(zip, { dir: config.serverPath })
+
+                console.log('📂 Restoring backup files');
+
+                config.doNotOverwrite.forEach(file => {
+                    fs.move(path.resolve('backup/', file), path.resolve(config.serverPath, file), { overwrite: true }, err => {
+                        if (err) return console.error(err);
+                        // console.log('Restored:', file);
+                    })
+                })
+                console.log('✅ Done');
+                
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        extraction();
+    })
+}).catch(err => {
+    console.log(err);
+})
